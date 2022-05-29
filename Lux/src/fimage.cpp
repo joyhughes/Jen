8c15bc23a130906fcdbd68b6884070ee1c6275f7
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-05-29 10:14:24 -0700

    Floating point image class

---
+++ Lux/src/fimage.cpp
@@ -0,0 +1,69 @@
+// Floating point image class using image template as a base
+
+#include "fimage.hpp"
+#include <memory>
+#include "image_loader.hpp"
+
+// pixel modification functions
+void fimage :: grayscale() {
+    std :: transform( base.begin(), base.end(), base.begin(),  []( frgb &f ) { return gray( f ); } );
+}
+
+void fimage :: load( const std :: string& filename ) {
+    reset();
+    image_loader loader( filename );
+    set_dim( { loader.xsiz, loader.ysiz } );
+
+	int c = 0;
+    frgb f;
+
+    for (auto it = begin (loader.img); it <= end (loader.img); ) {
+        if( loader.channels == 1 )	// monochrome image
+        {
+            setrc( f, *it );
+            setgc( f, *it );
+            setbc( f, *it );
+            it++;
+        }
+
+        if( loader.channels == 2 ) // don't know what to do in this case
+        {
+            setrc( f, *it );
+            setgc( f, *it );
+            setbc( f, *it );
+            it++; it++;
+        }
+
+        if( ( loader.channels == 3 ) | ( loader.channels == 4 ) )
+        {
+            setrc( f, *it );
+            it++;
+            setgc( f, *it );
+            it++;
+            setbc( f, *it );
+            it++;
+        }
+
+        // skip alpha channel - rgba ... if argb need to move line up
+        if( loader.channels == 4 ) it++;	
+        base.push_back( f );
+    }
+    mip_it();
+}
+
+void fimage :: quantize( std :: vector< unsigned char >& img )
+{
+    std :: for_each( base.begin(), base.end(), [ &img ]( frgb &f ) {
+        img.push_back( rc( f ) );
+        img.push_back( gc( f ) );
+        img.push_back( bc( f ) ); } );
+}
+
+void fimage :: write_jpg( const std :: string& filename, int quality ) {
+    std :: vector< unsigned char > img;
+	quantize( img );
+	wrapped_write_jpg( filename.c_str(), dim.x, dim.y, 3, img.data(), quality );
+}
+
+void fimage :: write_png( const std :: string& filename ) {
+}
---
+++ Lux/src/fimage.hpp
@@ -0,0 +1,22 @@
+#include "image.hpp"
+
+class fimage : public image< frgb > {
+
+protected:
+    void quantize( std::vector< unsigned char >& img );
+
+public:
+    fimage() : image() {}     
+    // creates image of particular size 
+    fimage( const vec2i& dims ) : image( dims ){}     
+    fimage( const vec2i& dims, const bb2f& bb ) : image( dims, bb ) {}     
+    // copy constructor
+    fimage( const I& img ) : image( img ) {}     
+    // pixel modification functions
+    void grayscale();
+
+    // I/O functions
+    void load(      const std::string& filename );
+    void write_jpg( const std::string& filename, int quality );
+    void write_png( const std::string& filename );
+};
