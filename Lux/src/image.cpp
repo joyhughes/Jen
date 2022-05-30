b1bbfe3823688d1ec90230f6498e547268d7955d
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-05-29 19:57:07 -0700

    Remove #define TT, add const to some parameters

--- Lux/src/image.cpp
+++ Lux/src/image.cpp
@@ -5,9 +5,8 @@
 #include "image.hpp"
 
 #define I image<T>
-#define TT template< class T >
 
-TT void I::mip_it() { // mip it good
+template< class T > void I :: mip_it() { // mip it good
     if( mip_me ) {
         if( !mipped ) {
             // allocate mip map memory
@@ -20,107 +19,107 @@ TT void I::mip_it() { // mip it good
     }
 }
 
-TT void I::de_mip() {  
+template< class T > void I :: de_mip() {  
     mipped = false;
     mip_utd = false;
     // deallocate all mip-maps
 }
 
-TT void I::reset() { 
+template< class T > void I :: reset() { 
     base.clear();
     set_dim( { 0, 0 } );
     de_mip(); 
 }
 
-TT void I::use_mip( bool m ) {
+template< class T > void I :: use_mip( bool m ) {
     mip_me = m;
     if( mip_me ) mip_it();
     else {} // free mip-map memory?
 }
 
-TT vec2i I::get_dim() { return dim; }
+template< class T > vec2i I :: get_dim() { return dim; }
 
-TT void I::set_dim( const vec2i& dims ) {
+template< class T > void I :: set_dim( const vec2i& dims ) {
     dim = dims;
     ipbounds.set( { 0, 0 }, dim );
 }
 
-TT bb2f I::get_bounds() { return bounds; }
-TT void I::set_bounds( const bounding_box< float, 2 >& bb ) { bounds = bb; }
+template< class T > bb2f I :: get_bounds() { return bounds; }
+template< class T > void I :: set_bounds( const bounding_box< float, 2 >& bb ) { bounds = bb; }
 
 // returns true if images have same dimensions
-TT bool I::compare_dims( const I& img ) { return ( dim == img.dim ); } 
+template< class T > bool I :: compare_dims( const I& img ) { return ( dim == img.dim ); } 
 
-TT void I::fill( const T& c ) {
-    std::fill( base.begin(), base.end(), c );
+template< class T > void I :: fill( const T& c ) {
+    std :: fill( base.begin(), base.end(), c );
     mip_it();
 }
 
-TT I& I::operator += ( I& rhs ) {
+template< class T > I& I :: operator += ( I& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), []( T &a, T &b) { return a + b; } );
-    mip_it();
-    return *this;
-}
-
-TT I& I::operator += ( const T& rhs ) {
-    using namespace linalg;
-    std::transform( base.begin(), base.end(), base.begin(), [ rhs ]( T &a ) { return a + rhs; } );
+    std :: transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), [] ( const T &a, const T &b ) { return a + b; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator -= ( I& rhs ) {
+template< class T > I& I :: operator += ( const T& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), []( T &a, T &b) { return a - b; } );
+    std :: transform( base.begin(), base.end(), base.begin(), [ rhs ]( const T &a ) { return a + rhs; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator -= ( const T& rhs ) {
+template< class T > I& I :: operator -= ( I& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), base.begin(), [ rhs ]( T &a ) { return a - rhs; } );
+    std :: transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), []( const T &a, const T &b ) { return a - b; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator *= ( I& rhs ) {
+template< class T > I& I :: operator -= ( const T& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), []( T &a, T &b) { return a * b; } );
+    std :: transform( base.begin(), base.end(), base.begin(), [ rhs ]( const T &a ) { return a - rhs; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator *= ( const T& rhs ) {
+template< class T > I& I :: operator *= ( I& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), base.begin(), [ rhs ]( T &a ) { return a * rhs; } );
+    std :: transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), [] ( const T &a, const T &b ) { return a * b; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator *= ( const float& rhs ) {
+template< class T > I& I :: operator *= ( const T& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), base.begin(), [ rhs ]( T &a ) { return a * rhs; } );
-    mip_it();
-    return *this;
-}
-TT I& I::operator /= ( I& rhs ) {
-    using namespace linalg;
-    std::transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), []( T &a, T &b) { return a / b; } );
+    std :: transform( base.begin(), base.end(), base.begin(), [ rhs ]( const T &a ) { return a * rhs; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator /= ( const T& rhs ) {
+template< class T > I& I :: operator *= ( const float& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), base.begin(), [ rhs ]( T &a ) { return a / rhs; } );
+    std :: transform( base.begin(), base.end(), base.begin(), [ rhs ]( const T &a ) { return a * rhs; } );
+    mip_it();
+    return *this;
+}
+template< class T > I& I :: operator /= ( I& rhs ) {
+    using namespace linalg;
+    std :: transform( base.begin(), base.end(), rhs.base.begin(), base.begin(), [] ( const T &a, const T &b ) { return a / b; } );
     mip_it();
     return *this;
 }
 
-TT I& I::operator /= ( const float& rhs ) {
+template< class T > I& I :: operator /= ( const T& rhs ) {
     using namespace linalg;
-    std::transform( base.begin(), base.end(), base.begin(), [ rhs ]( T &a ) { return a / rhs; } );
+    std :: transform( base.begin(), base.end(), base.begin(), [ rhs ]( const T &a ) { return a / rhs; } );
+    mip_it();
+    return *this;
+}
+
+template< class T > I& I :: operator /= ( const float& rhs ) {
+    using namespace linalg;
+    std :: transform( base.begin(), base.end(), base.begin(), [ rhs ]( const T &a ) { return a / rhs; } );
     mip_it();
     return *this;
 }
