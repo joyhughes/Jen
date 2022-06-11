8c03cdd08e3402bab59de12c9ceead648e786751
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-06-10 19:07:16 -0700

    Derived 32 bit image class

---
+++ Lux/src/uimage.hpp
@@ -0,0 +1,23 @@
+#include "ucolor.hpp"
+#include "image.hpp"
+
+class uimage : public image< ucolor > {
+
+protected:
+    void spool( std :: vector< unsigned char >& img );
+
+public:
+    uimage() : image() {}     
+    // creates image of particular size 
+    uimage( const vec2i& dims ) : image( dims ){}     
+    uimage( const vec2i& dims, const bb2f& bb ) : image( dims, bb ) {}     
+    // copy constructor
+    uimage( const I& img ) : image( img ) {}     
+    // pixel modification functions
+    void grayscale();
+
+    // I/O functions
+    void load(      const std :: string& filename );
+    void write_jpg( const std :: string& filename, int quality );
+    void write_png( const std :: string& filename );
+};
