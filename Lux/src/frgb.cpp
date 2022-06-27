35943c7a04f1531612a4a78553236c9c2dcd0ae2
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-06-27 14:56:01 -0700

    Remove commented out lines

--- Lux/src/frgb.cpp
+++ Lux/src/frgb.cpp
@@ -6,9 +6,9 @@
 
 static gamma_LUT glut( 2.2f );
 
-float r( const frgb &c )       { return c.R; }
-float g( const frgb &c )       { return c.G; }
-float b( const frgb &c )       { return c.B; }
+float rf( const frgb &c )       { return c.R; }
+float gf( const frgb &c )       { return c.G; }
+float bf( const frgb &c )       { return c.B; }
 
 // returns single bytes per component - assumes [0.0, 1.0] range
 // clip or constrain out of range values before using
@@ -19,11 +19,11 @@ unsigned char bc( const frgb &c ) {  return (unsigned char)glut.linear_to_SRGB( 
 // unsigned int ul() {} // bit shifty stuff
 
 // set component
-void setr( frgb &c, const float& r )   { c.R = r; }
-void setg( frgb &c, const float& g )   { c.G = g; }
-void setb( frgb &c, const float& b )   { c.B = b; }
-void set(  frgb &c, const float& r, const float& g, const float& b) { c.R = r;  c.G = g;  c.B = b; }
-frgb set(  const float& r, const float& g, const float& b ) { return { r, g, b }; }
+void setrf( frgb &c, const float& r )   { c.R = r; }
+void setgf( frgb &c, const float& g )   { c.G = g; }
+void setbf( frgb &c, const float& b )   { c.B = b; }
+void setf(  frgb &c, const float& r, const float& g, const float& b) { c.R = r;  c.G = g;  c.B = b; }
+frgb setf(  const float& r, const float& g, const float& b ) { return { r, g, b }; }
 
 // TODO - set from bracketed list
 
@@ -40,20 +40,17 @@ frgb setc( const unsigned char& r, const unsigned char& g, const unsigned char& 
 // Clip to range [ 0.0, 1.0 ] but keep colors in proportion
 void constrain( frgb &c ) {
     // find maximum color
-    float max = linalg::maxelem( c );
-    if( max > 1.0f ) {
+    float maxc = linalg::maxelem( c );
+    if( maxc > 1.0f ) {
         // bring negative colors to zero
-        linalg::clamp( c, 0.0f, max );
+        linalg::clamp( c, 0.0f, maxc );
         // if max color > 1.0 divide all colors by max
-        c /= max;
+        c /= maxc;
     }
 }
 
-void apply_mask( frgb &result, const frgb &base, const frgb &mask ) {
-    result = linalg::lerp( result, base, mask );
-    /*result.R = result.R * mask.R + base.R * ( 1.0 - mask.R);
-    result.G = result.G * mask.G + base.G * ( 1.0 - mask.G);
-    result.B = result.B * mask.B + base.B * ( 1.0 - mask.B);*/
+void apply_mask( frgb &result, const frgb &layer, const frgb &mask ) {
+    result = linalg::lerp( result, layer, mask );
 }
 
 void print_SRGB( const frgb &c ) {
