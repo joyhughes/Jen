271788250d2a42e25e2dc9b483a852eaebd43b54
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-06-27 14:13:24 -0700

    Add vector field class

---
+++ Lux/src/vector_field.cpp
@@ -0,0 +1,153 @@
+#include "linalg.h"
+#include "vect2.hpp"
+#include "vector_field.hpp"
+
+void vortex_field::generate() {
+    vortex_params vort;
+    std::uniform_int_distribution<> velocity_distribution( min_velocity, max_velocity );
+
+    generated = true;
+    for( int i = 0; i < n; i++ ) {
+        vort.diameter =  rand_range( min_diameter,  max_diameter );
+        vort.soften =    rand_range( min_soften,    max_soften );
+        vort.revolving = revolving;
+
+        vort.center_of_revolution = bounds.box_of_random();
+        if( revolving ) vort.center_orig = vort.center_of_revolution + 
+            linalg::rot( rand_range( 0.0f, TAU ), { 0.0f, rand_range( min_orbital_radius, max_orbital_radius ) } );
+        else vort.center_orig = vort.center_of_revolution;
+        if( revolving ) {
+            vort.velocity = velocity_distribution( gen );
+            switch ( velocity_direction )
+            {
+            case COUNTERCLOCKWISE:
+                vort.velocity = abs( vort.velocity );
+                break;
+
+            case CLOCKWISE:
+                vort.velocity = -abs( vort.velocity );
+                break;
+            
+            case RANDOM:
+                if( rand_bit( gen ) ) vort.velocity *= -1;
+                break;
+
+            case LAVA_LAMP:
+                if( vort.center_of_revolution.x < ( bounds.b1.x + bounds.b2.x ) / 2.0f ) vort.intensity *= -1;     
+                break;
+            }
+        }
+
+        vort.intensity = rand_range( min_intensity, max_intensity );
+        switch ( intensity_direction )
+        {
+        case COUNTERCLOCKWISE:
+            vort.intensity = abs( vort.intensity );
+            break;
+
+        case CLOCKWISE:
+            vort.intensity = -abs( vort.intensity );
+            break;
+        
+       case RANDOM:
+            if( rand_bit( gen ) ) vort.intensity *= -1.0f;
+            break;
+
+        case LAVA_LAMP:
+            if( vort.center_of_revolution.x < ( bounds.b1.x + bounds.b2.x ) / 2.0f ) vort.intensity *= -1.0f;     
+            break;
+        }
+
+        vorts.push_back( vort );
+    }
+}
+
+// Use Newton's method to move along flow line proportional to step value
+// Angle in degrees
+vec2f vector_field::advect( const vec2f& v, const float& step, const float& angle, const bool& smooth, const image_extend& extend )
+{
+    if( angle == 0.0f ) return v +                                    sample( v, smooth, extend )   * step;
+    else                return v + linalg::rot( angle / 360.0f * TAU, sample( v, smooth, extend ) ) * step;
+}
+
+// This overload receives a matrix parameter - more efficient if invoked repeatedly with the same angle
+vec2f vector_field::advect( const vec2f& v, const float& step, const mat2f& m, const bool& smooth, const image_extend& extend )
+{
+    return v + linalg::mul( m, sample( v, smooth, extend ) ) * step;
+}
+
+void vector_field::complement() { for( auto& v : base ) { v = ::complement( v ); } mip_it(); }
+void vector_field::radial()     { for( auto& v : base ) { v = ::radial( v );     } mip_it(); }
+void vector_field::cartesian()  { for( auto& v : base ) { v = ::cartesian( v );  } mip_it(); }
+
+void vector_field::rotate_vectors( const float& ang ) {
+    mat2f m = linalg::rotation_matrix_2D( ang / 360.0f * TAU );
+    for( auto& v : base ) { v = linalg::mul( m, v ); }
+    mip_it();
+}
+
+void vector_field::normalize() { std::transform( base.begin(), base.end(), base.begin(), [] ( const vec2f& v ) { return linalg::normalize( v );  } ); mip_it();}
+
+void vector_field::inverse( float diameter, float soften ) {
+    if( diameter == 0.0f ) { fill( { 0.0f, 0.0f } ); }
+    else for( auto& v : base ) { v = ::inverse( v, diameter, soften ); }
+    mip_it();
+}
+
+void vector_field::inverse_square( float diameter, float soften ) {
+    if( diameter == 0.0f ) { fill( { 0.0f, 0.0f } ); }
+    else for( auto& v : base ) { v = ::inverse_square( v, diameter, soften ); }
+    mip_it();
+}
+
+void vector_field::concentric( const vec2f& center ) { 
+    position_fill();
+    for( auto& v : base ) { v = v - center; }
+    mip_it(); 
+}
+
+void vector_field::rotation( const vec2f& center ) { 
+    concentric();
+    complement();
+}
+
+void vector_field::spiral( const vec2f& center, const float& cscale, const float& rscale )
+{
+    vector_field buffer( *this );
+
+    concentric();
+    *this *= cscale;
+    buffer.rotation();
+    buffer *= rscale;
+    *this += buffer;
+    mip_it();
+}
+
+void vector_field::vortex( const vortex_params& vort, const float& t ) {
+    vec2f center= vort.center_orig;
+    if( vort.revolving ) center = vort.center_of_revolution + linalg::rot( vort.velocity * t * TAU, vort.center_orig - vort.center_of_revolution );
+    rotation( center );
+    inverse( vort.diameter, vort.soften );
+    *this *= vort.intensity;
+    mip_it();
+}
+
+void vector_field::turbulent( vortex_field& f, const float& t ) {
+    if( !f.generated ) f.generate();
+    fill( { 0.0f, 0.0f } );
+    vector_field buffer( *this );
+    for( auto& vort : f.vorts ) { buffer.vortex( vort ); *this += buffer; }
+    mip_it();
+}
+ 
+void vector_field::position_fill() { 
+    auto v = base.begin();
+    for( int y = 0; y < dim.y; y++ ) {
+        for( int x = 0; x < dim.x; x++ ) {
+            *v = bounds.bb_map( { x, y }, ipbounds );
+            v++;
+        }
+    }
+    // rather than using mip_it() here, calculate directly up hierarchy using bounding box
+}
+
---
+++ Lux/src/vector_field.hpp
@@ -0,0 +1,86 @@
+#ifndef __VECTOR_FIELD_HPP
+#define __VECTOR_FIELD_HPP
+
+#include "image.hpp"
+
+struct vortex_params {
+    float diameter;   // float - Overall size of vortex
+    float soften;     // float - Avoids a singularity in the center of vortex
+    float intensity;  // float - Strength of vortex. How vortexy is it? Negative value swirls the opposite direction.
+    vec2f center_orig; // vect2 - Initial position of vortex
+    // other mathematical properties here ... vortex type?  ( inverse, inverse_square, donut, etc )
+
+    // Animation parameters
+    bool revolving;  // bool - does the vortex revolve around a center?
+    int  velocity;   // float - Speed of revolution. Must be integer for animation to loop
+    vec2f center_of_revolution;   // vect2 - vortex revolves around this point
+
+    vortex_params( float diam = 0.5f, float soft = 0.25f, float inten = 1.0f, vec2f c_orig = { 0.0f, 0.0f } ) :
+        diameter( diam ), soften( soft ), intensity( inten ), center_orig( c_orig ),
+        revolving( false ), velocity( 0 ), center_of_revolution( { 0.0f, 0.0f } ) {}
+
+    vec2f& eval( const vec2f& v );
+};
+
+struct vortex_field  {                  // parameters for field of vortices
+    int n;                              // number of vortices in field    
+    bb2f bounds;
+    float scale_factor;                 // overall scaling factor
+
+    float min_diameter, max_diameter;   // float - size of vortex
+    float min_soften, max_soften;       // float - singularity avoidance
+    float min_intensity, max_intensity; // float
+    rotation_direction intensity_direction;  // object of RotationDirection
+        // vortex type?  ( inverse, inverse_square, donut, etc )
+
+    bool revolving;                     // do vortices revolve?
+    int min_velocity, max_velocity;           // must be integer values for animation to loop
+    rotation_direction velocity_direction;   // object of RotationDirection
+    float min_orbital_radius, max_orbital_radius; // float
+
+    std::vector< vortex_params > vorts;
+    bool generated;                     // has this.generate() been run?
+
+    vortex_field( int num = 10, bool rev = true ) : n( num ), scale_factor( 0.5f ),
+        min_diameter( 0.5f ), max_diameter( 0.5f ), min_soften( 0.25f ), max_soften( 0.25f ),
+        min_intensity( 1.0f ), max_intensity( 1.0f ), intensity_direction( RANDOM ),
+        revolving( rev ), min_velocity( 1 ), max_velocity( 1 ), velocity_direction( RANDOM ),
+        min_orbital_radius( 0.0f ), max_orbital_radius( 0.5f ), generated( false )  {}
+   
+    void generate();
+};
+
+class vector_field : public image< vec2f > {
+
+public:
+    vector_field() : image() {}     
+    // creates image of particular size 
+    vector_field( const vec2i& dims ) : image( dims ){}     
+    vector_field( const vec2i& dims, const bb2f& bb ) : image( dims, bb ) {}     
+    // copy constructor
+    vector_field( const I& img ) : image( img ) {}     
+
+    vec2f advect( const vec2f& v, const float& step, const float& angle = 0.0f, const bool& smooth = true, const image_extend& extend = SAMP_REPEAT );
+    vec2f advect( const vec2f& v, const float& step, const mat2f& m,            const bool& smooth = true, const image_extend& extend = SAMP_REPEAT );
+
+    void complement();
+    void radial();
+    void cartesian();
+
+    void rotate_vectors( const float& ang );
+    void normalize();
+    void inverse( float diameter, float soften = 0.0f );
+    void inverse_square( float diameter, float soften = 0.0f );
+
+    void concentric( const vec2f& center = { 0.0f, 0.0f } );
+    void rotation(   const vec2f& center = { 0.0f, 0.0f } );
+    void spiral(     const vec2f& center = { 0.0f, 0.0f }, const float& cscale = 1.0f, const float& rscale = 1.0f );
+
+    void vortex( const vortex_params& vort, const float& t = 0.0f );
+    void turbulent( vortex_field& f,  const float& t = 0.0f );
+
+    void position_fill();
+
+};
+
+#endif // __VECTOR_FIELD_HPP
