71eab4bca96d255fe5ce05b33915127953cc3744
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-08-22 17:03:15 -0700

    Enable mask in `splat()`
    Handle situation where mask and splat images have different dimensions

--- Lux/src/image.hpp
+++ Lux/src/image.hpp
@@ -12,6 +12,8 @@
 #include <vector>
 #include <memory>
 #include <optional>
+#include <functional>
+//#include "any_image.hpp"
 
 enum image_extend
 {
@@ -19,6 +21,7 @@ enum image_extend
 	SAMP_REPEAT,
 	SAMP_REFLECT
 };
+
 typedef enum image_extend image_extend;
 
 // Root template for raster-based data
@@ -28,7 +31,7 @@ protected:
     typedef image< T > I;
 
     vec2i dim;              // Dimensions in pixels
-    std :: vector< T > base;  // Pixels
+    std::vector< T > base;  // Pixels
 
     // bounding boxes
     bb2f bounds;      // bounding box in linear space
@@ -39,9 +42,9 @@ protected:
     bool mip_me;      // Use mip-mapping for this image? Default false.
     bool mipped;      // has mip-map been allocated?
     bool mip_utd;     // is mip-map up to date? Set to false with any modification of base image
-    std :: vector< std :: unique_ptr< std :: vector< T > > > mip;  // mip-map of image
-    std :: vector< std :: unique_ptr< bb2i > > ipbounds_mip;  // pixel space bounding box of mipped image (int)
-    std :: vector< std :: unique_ptr< bb2f > > fpbounds_mip;  // pixel space bounding box of mipped image (float)
+    std::vector< std::unique_ptr< std::vector< T > > > mip;  // mip-map of image
+    std::vector< std::unique_ptr< bb2i > > ipbounds_mip;  // pixel space bounding box of mipped image (int)
+    std::vector< std::unique_ptr< bb2f > > fpbounds_mip;  // pixel space bounding box of mipped image (float)
     // resamples image to crate mip-map            
     void de_mip();  // deallocate all mip-maps
     void mip_it();  // mipit good
@@ -63,9 +66,12 @@ public:
     image( const I& img ) : dim( img.dim ), bounds( img.bounds ), ipbounds( img.ipbounds ), fpbounds( ipbounds ), 
         mip_me( img.mip_me ), mipped( img.mipped ), mip_utd( img.mip_utd ) 
         {
-            std :: copy( img.base.begin(), img.base.end(), back_inserter( base ) );
+            std::copy( img.base.begin(), img.base.end(), back_inserter( base ) );
             mip_it();
-        }     
+        }
+
+    // load constructor
+    image( const std::string& filename ) : image() { load( filename ); }     
 
     //typedef std::iterator< std::forward_iterator_tag, std::vector< T > > image_iterator;
     auto begin() noexcept { return base.begin(); }
@@ -78,6 +84,7 @@ public:
     const vec2i get_dim() const;
     void set_dim( const vec2i& dims );
     const bb2f get_bounds() const;
+    const bb2i get_ipbounds() const;
     void set_bounds( const bb2f& bb );
     template< class U > bool compare_dims( const image< U >& img ) const { return ( dim == img.get_dim() ); }  // returns true if images have same dimensions
 
@@ -94,21 +101,27 @@ public:
     // size modification functions
     void resize( vec2i siz );
     void crop( const bb2i& bb );
-    void circle_crop( float ramp_width = 0.0f );	// Sets to zero everything outside of a centered circle
+    void circle_crop( const float& ramp_width = 0.0f );	// Sets to zero everything outside of a centered circle
 
     // pixel modification functions
     void fill( const T& c );
 
     // masking
-    void apply_mask( const I& layer, const I& mask );
+    void apply_mask( const I& layer, const I& mask, const mask_mode& mmode = MASK_BLEND );
+
+    typedef std::optional< std::reference_wrapper< T > > opt_pix_ref;
+    typedef std::optional< std::reference_wrapper< image< T > > > opt_img_ref;
 
     // rendering
     void splat( 
-        const vec2f& center, 			// coordinates of splat center
-        const float& scale, 			// radius of splat
-        const float& theta, 			// rotation in degrees
-        std::optional< T >& tint,		// change the color of splat
-        const I& g 	);		            // image of the splat
+        const vec2f& center, // coordinates of splat center
+        const float& scale,  // radius of splat
+        const float& theta,  // rotation in degrees
+        const opt_img_ref g = std::nullopt,    // image of the splat
+        const opt_img_ref mask = std::nullopt, // optional mask image
+        const opt_pix_ref tint = std::nullopt, // change the color of the splat
+        const mask_mode& mmode = MASK_BLEND    // how will mask be applied to splat and backround?
+    );  
 
     void warp ( const image< T >& in, 
                 const image< vec2f >& vf, 
@@ -116,6 +129,19 @@ public:
                 const bool& smooth = false, 
                 const bool& relative = true,
                 const image_extend& extend = SAMP_SINGLE );
+
+    void warp ( const image< T >& in, 
+                const std::function< vec2f( vec2f ) >& vfn, 
+                const float& step = 1.0f, 
+                const bool& smooth = false, 
+                const bool& relative = true,
+                const image_extend& extend = SAMP_SINGLE );
+
+    // load image from file - JPEG and PNG are only defined for fimage and uimage, so virtual function
+    // future - binary file type for any image (needed for vector field and out of range fimage)
+    virtual void load( const std::string& filename ) {}
+
+    void apply( const std::function< T ( const T&, const float& ) > fn, const float& t = 0.0f );
 
     // operators
     I& operator += ( I& rhs );
@@ -129,14 +155,49 @@ public:
     I& operator /= ( const T& rhs );
     I& operator /= ( const float& rhs );
 
+    I& operator () ();
     //void apply( unary_func< T > func ); // apply function to each pixel (in place)
     //void apply( unary_func< T > func, image< T >& result ); // apply function to each pixel (to target image)
     // apply function to two images
 };
 
-struct image_mask_pair {
-    
-    bool has_mask();
+// Used for double-buffered rendering. Owns pointer to image - makes a duplicate when 
+// double-buffering is needed for an effect. Can be used in effect-component stacks or for 
+// persistent effects such as CA, melt, or hyperspace
+template< class T > class buffer_pair {
+    typedef std::unique_ptr< image< T > > image_ptr;
+    std::pair< image_ptr, image_ptr > image_pair;
+public:
+    bool has_image() { return image_pair.first.get() != NULL; }
+    image< T >& get_image() { return *image_pair.first; }
+    image< T >& get_buffer() {
+        if( image_pair.second.get() == NULL ) image_pair.second.reset( new image< T >( *image_pair.first ) );
+        return *image_pair.second;
+    }
+    void swap() { image_pair.first.swap( image_pair.second ); }
+    void load( const std::string& filename ) { 
+        image_pair.first.reset( new image< T >( filename ) );
+        image_pair.second.reset( NULL );
+    }
+    void reset( const image< T >& img ) { 
+        image_pair.first.reset( new image< T >( img ) );
+        image_pair.second.reset( NULL );
+    }
+
+    image< T >& operator () () { return get_image(); }
+
+    buffer_pair() : image_pair( NULL, NULL ) {}
+    buffer_pair( const std::string& filename ) { load( filename ); }
+    buffer_pair( const image< T >& img ) { reset( img ); }
 };
+
+// Owns a buffer pair for image, buffer pair for mask
+// Image class does not have alpha channel - instead uses 3-channel masking
+/*template< class T, class U > struct image_mask_pair {
+    std::unique_ptr< image< T > > img;
+    std::unique_ptr< image< T > > mask;
+
+    bool has_mask();
+}; */
 
 #endif // __IMAGE_HPP
