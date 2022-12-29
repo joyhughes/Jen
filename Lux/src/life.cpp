e8017a8e44ad250f697d6b7ff975bd5843b6569e
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2022-12-29 10:58:35 -0800

    Add Margolis neighborhood
    Square four-cell neighborhood, offset on alternate frames. Neighborhood is randomly rotated, producing a diffusion effect.

--- Lux/src/life.cpp
+++ Lux/src/life.cpp
@@ -14,6 +14,19 @@
 #define DM neighbors[7]
 #define DR neighbors[8]
 
+// Von Neumann neighborhood shortcuts
+#define VNU neighbors[0]
+#define VNL neighbors[1]
+#define VNM neighbors[2]
+#define VNR neighbors[3]
+#define VND neighbors[4]
+
+// Margolis neighborhood shortcuts
+#define MUL neighbors[0]
+#define MUR neighbors[1]
+#define MLL neighbors[2]
+#define MLR neighbors[3]
+
 // future - implement multiresolution rule on mip-map
 
 // Uses toroidal boundary conditions
@@ -22,9 +35,6 @@ template< class T > bool CA< T >::operator() ( buffer_pair<T> &buf, const float 
     auto in =  buf.get_image().begin();
     auto out = buf.get_buffer().begin();
     vec2i dim = buf.get_image().get_dim();
-
-    unsigned int on  = 0xffffffff;
-    unsigned int off = 0xff000000;
 
     // check neighborhood type
     if( neighborhood == NEIGHBORHOOD_MOORE ) {
@@ -82,8 +92,72 @@ template< class T > bool CA< T >::operator() ( buffer_pair<T> &buf, const float 
         }
 
     } else if( neighborhood == NEIGHBORHOOD_VON_NEUMANN ) {
-    } else if( neighborhood == NEIGHBORHOOD_MARGOLIS ) {
+    } else if( neighborhood == NEIGHBORHOOD_MARGOLIS ) { // Works best if image dimensions are multiples of 2
+        neighbors.resize( 4 );
+        result.resize( 4 );
+        auto out_ul = out; auto out_ur = out; auto out_ll = out; auto out_lr = out;
+        auto in_ul = in; auto in_ur = in; auto in_ll = in; auto in_lr = in;
+        // initialize iterators
+        int startx, starty;
+        if( frame % 2 ) { startx = 0; starty = 0; } // even frame - fits into upper left corner of image
+        else            { startx = 1; starty = -1; } // odd frame - offset by 1 (initally straddles four corners of image)
+        // scan through image by row
+        for( int y = starty; y < dim.y - 1; y+=2 ) {
+            if( frame % 2 ) { // even left edge
+                out_ul = out + y * dim.x;       out_ur = out + y * dim.x + 1; 
+                out_ll = out + (y + 1) * dim.x; out_lr = out + (y + 1) * dim.x + 1;
+
+                in_ul = in + y * dim.x;         in_ur = in + y * dim.x + 1; 
+                in_ll = in + (y + 1) * dim.x;   in_lr = in + (y + 1) * dim.x + 1;
+            }
+            else { // odd left edge
+                if( y == -1 ) { // top row
+                    out_ul = out + dim.y * dim.x - 1; out_ur = out + ( dim.y - 1 ) * dim.x; 
+                    out_ll = out + dim.x - 1;         out_lr = out;
+
+                    in_ul  = in  + dim.y * dim.x - 1; in_ur  = in  + ( dim.y - 1 ) * dim.x; 
+                    in_ll  = in  + dim.x - 1;         in_lr  = in;
+
+                    MUL = *in_ul; MUR = *in_ur; MLL = *in_ll; MLR = *in_lr;
+                    rule( neighbors, result );  // apply rule
+                    *out_ul = result[0]; *out_ur = result[1]; *out_ll = result[2]; *out_lr = result[3];
+
+                    out_ul = out + ( dim.y - 1 ) * dim.x + 1; out_ur = out + ( dim.y - 1 ) * dim.x + 2; 
+                    out_ll = out + 1;                         out_lr = out + 2;
+
+                    in_ul  = in +  ( dim.y - 1 ) * dim.x + 1; in_ur  = in  + ( dim.y - 1 ) * dim.x + 2;
+                    in_ll  = in  + 1;                         in_lr  = in  + 2;
+                }
+                else {
+                    out_ul = out + (y + 1) * dim.x - 1; out_ur = out + y * dim.x; 
+                    out_ll = out + (y + 2) * dim.x - 1; out_lr = out + (y + 1) * dim.x;
+
+                    in_ul = in + (y + 1) * dim.x - 1;   in_ur = in + y * dim.x;
+                    in_ll = in + (y + 2) * dim.x - 1;   in_lr = in + (y + 1) * dim.x;
+
+                    MUL = *in_ul; MUR = *in_ur; MLL = *in_ll; MLR = *in_lr;
+                    rule( neighbors, result );  // apply rule
+                    *out_ul = result[0]; *out_ur = result[1]; *out_ll = result[2]; *out_lr = result[3];
+
+                    out_ul = out + y * dim.x + 1;       out_ur = out + y * dim.x + 2; 
+                    out_ll = out + (y + 1) * dim.x + 1; out_lr = out + (y + 1) * dim.x + 2;
+
+                    in_ul = in + y * dim.x + 1;         in_ur = in + y * dim.x + 2;
+                    in_ll = in + (y + 1) * dim.x + 1;   in_lr = in + (y + 1) * dim.x + 2;
+                }
+            }
+            for( int x= startx; x < dim.x; x += 2 ) {
+                // set neighborhood
+                MUL = *in_ul; MUR = *in_ur; MLL = *in_ll; MLR = *in_lr;
+                rule( neighbors, result );  // apply rule
+                *out_ul = result[0]; *out_ur = result[1]; *out_ll = result[2]; *out_lr = result[3];
+                // update neighborhood
+                in_ul  += 2; in_ur  += 2; in_ll  += 2; in_lr  += 2;
+                out_ul += 2; out_ur += 2; out_ll += 2; out_lr += 2;
+            }
+        }
     } 
+    frame++;
     buf.swap();
     return true;
 }
@@ -95,14 +169,10 @@ template< class T > bool CA< T >::operator() ( buffer_pair<T> &buf, const float 
  *  http://www.bitstorm.org/gameoflife/lexicon/
  */
 
-template<class T> void life<T>::operator()(const std::vector<T>& neighbors, std::vector<T>& result) {    
+template< class T > void life< T >::operator () (const std::vector<T>& neighbors, std::vector<T>& result) {    
     int count = 0;
-    for( int i=0; i<4; i++ ) {
-        if( neighbors[ i ] == on ) count++;
-    }    
-    for( int i=5; i<9; i++ ) {
-        if( neighbors[ i ] == on ) count++;
-    }
+    for( int i = 0; i < 4; i++ ) { count += (neighbors[ i ] == on); }    
+    for( int i = 5; i < 9; i++ ) { count += (neighbors[ i ] == on); } 
     if( MM == on ) {
         if( count == 2 || count == 3 ) result[0] = on;
         else result[0] = off;
@@ -112,12 +182,23 @@ template<class T> void life<T>::operator()(const std::vector<T>& neighbors, std:
     }
 }
 
-//template class CA< frgb >;       // fimage
+template< class T > void diffuse< T >::operator () (const std::vector<T> &neighbors, std::vector<T> &result) {
+    int r = rand_4( gen );
+    result[0] = neighbors[ r ];
+    result[1] = neighbors[ (r + 1) % 4 ];
+    result[2] = neighbors[ (r + 2) % 4 ];
+    result[3] = neighbors[ (r + 3) % 4 ];
+}
+
+template class CA< frgb >;       // fimage
 template class CA< ucolor >;     // uimage
-//template class CA< vec2f >;      // vector_field
+template class CA< vec2f >;      // vector_field
 
 template class life< frgb >;       // fimage
 template class life< ucolor >;     // uimage
 template class life< vec2f >;      // vector_field
 
+template class diffuse< frgb >;       // fimage
+template class diffuse< ucolor >;     // uimage
+template class diffuse< vec2f >;      // vector_field
 
