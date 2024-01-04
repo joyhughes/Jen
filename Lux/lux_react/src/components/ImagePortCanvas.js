a35e181af552e952e1e14e250a616bcbf9a820f5
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2024-01-03 20:59:42 -0800

    Add mouse interaction

--- Lux/lux_react/src/components/ImagePortCanvas.js
+++ Lux/lux_react/src/components/ImagePortCanvas.js
@@ -4,7 +4,50 @@ function ImagePortCanvas( { width, height } ) {
   const canvasRef = useRef(null);
   const [isModuleReady, setModuleReady] = useState(false);
 
-const updateCanvas = useCallback(async () => {
+  const handleMouseDown = useCallback(() => {
+    if (window.Module) {
+      window.Module.mouse_down(true);
+    }
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    if (window.Module) {
+      window.Module.mouse_down(false);
+    }
+  }, []);
+
+  const handleMouseEnter = useCallback(() => {
+    if (window.Module) {
+      window.Module.mouse_over(true);
+    }
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    if (window.Module) {
+      window.Module.mouse_over(false);
+    }
+  }, []);
+
+  const handleMouseMove = useCallback((event) => {
+    if (window.Module) {
+      const rect = canvasRef.current.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      //console.log( "handleMouseMove x=" + x + " y=" + y + " width=" + width + " height=" + height );
+      window.Module.mouse_move(x, y, width, height );
+      window.Module.mouse_over( true );
+    }
+  }, [ width, height ] );
+
+  const handleMouseClick = useCallback(() => {
+    if (window.Module && canvasRef.current) {
+      console.log( "handleMouseClick" );
+      window.Module.mouse_click( true );
+      window.Module.mouse_over(  true );
+    }
+  }, [] );
+
+  const updateCanvas = useCallback(async () => {
     const ctx = canvasRef.current.getContext('2d');
 
     const imageDataArray = window.Module.get_img_data();
@@ -17,12 +60,14 @@ const updateCanvas = useCallback(async () => {
       bufHeight
     );
     const imageBitmap = await createImageBitmap(imageData);
+    //console.log( "width=" + width + " height=" + height );
     ctx.drawImage(imageBitmap, 0, 0, width, height);
 
   }, [width, height]);
 
   useEffect(() => {
     if (window.Module) {
+      window.Module.set_frame_callback(updateCanvas);
       setModuleReady(true);
     } else {
       // Poll for the Module to be ready
@@ -38,6 +83,26 @@ const updateCanvas = useCallback(async () => {
     }
   }, [updateCanvas]);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.addEventListener('mousemove', handleMouseMove);
+      canvas.addEventListener('click', handleMouseClick);
+      canvas.addEventListener('mousedown', handleMouseDown);
+      canvas.addEventListener('mouseup', handleMouseUp);
+      canvas.addEventListener('mouseenter', handleMouseEnter);
+      canvas.addEventListener('mouseleave', handleMouseLeave);
+
+      return () => {
+        canvas.removeEventListener('mousemove', handleMouseMove);
+        canvas.removeEventListener('click', handleMouseClick);
+        canvas.removeEventListener('mousedown', handleMouseDown);
+        canvas.removeEventListener('mouseup', handleMouseUp);
+        canvas.removeEventListener('mouseenter', handleMouseEnter);
+        canvas.removeEventListener('mouseleave', handleMouseLeave);
+      };
+    }
+  }, [handleMouseMove, handleMouseClick, handleMouseDown, handleMouseUp, handleMouseEnter, handleMouseLeave]);
 
   return (
       <canvas ref={canvasRef} width={width} height={height}></canvas>
