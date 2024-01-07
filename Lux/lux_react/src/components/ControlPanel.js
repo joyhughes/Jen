abead5a69bdde4d3d5249633c9a4561203e08880
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2024-01-07 10:27:15 -0800

    Set slider props from C++

--- Lux/lux_react/src/components/ControlPanel.js
+++ Lux/lux_react/src/components/ControlPanel.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import Paper from '@mui/material/Paper';
 import MediaController from './MediaController';
 
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+
 import Slider from '@mui/material/Slider';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
@@ -10,10 +14,23 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 
+import { useTheme } from '@mui/material/styles';
+import { styled } from '@mui/system';
+
 //import Module from './useEmscripten';
 
 function ControlPanel( { ratio, panelSize } ) {
-  const [sliderValue, setSliderValue] = useState(30);
+  const theme = useTheme();
+
+  const [sliderProps, setSliderProps] = useState({
+    label: '',
+    value: 0,
+    min: 0,
+    max: 100,
+    step: 1,
+    ready: false
+  });
+
   const [radioValue, setRadioValue] = useState('option1');
   const [textValue, setTextValue] = useState('');
   const [dimensions, setDimensions] = useState({ width: 0, height: 0, isRowDirection: true });
@@ -42,8 +59,46 @@ function ControlPanel( { ratio, panelSize } ) {
     return () => window.removeEventListener("resize", resizeBox);
   }, [ ratio, panelSize ]);
 
+  useEffect(() => {
+    if (window.Module) {
+      setSliderProps({
+        label: window.Module.get_slider_label(),
+        value: window.Module.get_slider_value(),
+        min:   window.Module.get_slider_min(),
+        max:   window.Module.get_slider_max(),
+        step:  window.Module.get_slider_step(),
+        ready: true
+      });
+      console.log( "ControlPanel useEffect mainSlider=" + JSON.stringify( sliderProps ) );
+
+    } else {
+      // Poll for the Module to be ready
+      const intervalId = setInterval(() => {
+        if (window.Module) {
+          setSliderProps({
+            label: window.Module.get_slider_label(),
+            value: window.Module.get_slider_value(),
+            min:   window.Module.get_slider_min(),
+            max:   window.Module.get_slider_max(),
+            step:  window.Module.get_slider_step(),
+            ready: true
+          });
+          console.log( "ControlPanel useEffect mainSlider=" + JSON.stringify( sliderProps ) );
+          clearInterval(intervalId);
+        }
+      }, 100); // Check every 100ms
+
+      return () => clearInterval(intervalId);
+    }
+  }, [] );
+
   const handleSliderChange = (event, newValue) => {
-    setSliderValue(newValue);
+    console.log( "ControlPanel handleSliderChange newValue=" + newValue );
+    setSliderProps(prevProps => ({
+      ...prevProps,
+      value: newValue
+    }));
+  
     if (window.Module) {
       window.Module.slider_value(newValue);
     }
@@ -56,7 +111,7 @@ function ControlPanel( { ratio, panelSize } ) {
   const handleTextChange = (event) => {
     setTextValue(event.target.value);
   };
-
+  
   return (
     <Paper elevation={3} 
       sx={{ 
@@ -92,15 +147,29 @@ function ControlPanel( { ratio, panelSize } ) {
           height: 60,  // Or any desired height for the enclosing container
         }}
       >
-        <Slider 
-          style={{ width: panelSize - 40 }}
-          value={ sliderValue } 
-          onChange={ handleSliderChange }
-          aria-labelledby = "input-slider"
-        /> 
+        <Stack spacing={0} direction="column" alignItems="center">
+          <Slider 
+            size="small"
+            style={{ width: panelSize - 40 }}
+            min={sliderProps.min}
+            max={sliderProps.max}
+            step={sliderProps.step}
+            value={sliderProps.value}
+            onChange={handleSliderChange}
+            aria-labelledby='input-slider'
+            aria-label={sliderProps.label}
+            valueLabelDisplay="auto" // Shows the value label on hover or focus
+            />
+          {sliderProps.label && (
+            <Typography style={{ textAlign: 'center', color: theme.palette.primary.main }}>
+              {sliderProps.label}
+            </Typography>
+          )}
+        </Stack>
       </Paper>
     </Paper>
   );
+  
 }
 
 export default ControlPanel;
