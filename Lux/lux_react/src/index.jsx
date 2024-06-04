218cfdb816501608eff689c28fef087cc3f533c5
Joy Hughes <solargardens@outlook.com>
2024-06-03 19:06:40 -0700

    Enable Emscripten
    Scene loaded, however no widgets are rendered and nothing displayed on canvas

--- Lux/lux_react/src/index.jsx
+++ Lux/lux_react/src/index.jsx
@@ -7,14 +7,19 @@ import App from './App.jsx';
 var factory = require('./lux.js');
 
 factory().then((instance) => {
-  window.Module = instance; // Assign to global variable
-  window.Module.canvas = document.getElementById('imagePortCanvas');
+  window.module = instance; // Assign to global variable
+  window.module.canvas = document.getElementById('imagePortCanvas');
 });
 */
 
-import('./lux.js').then((instance) => {
-  window.module = instance; // Assign to global variable
-//  window.module.canvas = document.getElementById('imagePortCanvas');
+
+await import('./lux.js').then((module) => {
+  const factory = module.default ? module.default : module;
+  
+  factory().then((instance) => {
+    window.module = instance; // Assign to global variable
+    window.module.canvas = document.getElementById('imagePortCanvas');
+  });
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
