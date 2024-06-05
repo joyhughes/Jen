b77fcf5c06bbcceeea59b3138fd037e44484dc0a
Joy Hughes <solargardens@outlook.com>
2024-06-04 18:23:04 -0700

    Wrap top level await in async function

--- Lux/lux_react/src/index.jsx
+++ Lux/lux_react/src/index.jsx
@@ -12,15 +12,20 @@ factory().then((instance) => {
 });
 */
 
-
-await import('./lux.js').then((module) => {
-  const factory = module.default ? module.default : module;
-  
-  factory().then((instance) => {
-    window.module = instance; // Assign to global variable
-    window.module.canvas = document.getElementById('imagePortCanvas');
-  });
-});
+(async () => {
+  try {
+    await import('./lux.js').then((module) => {
+      const factory = module.default ? module.default : module;
+      
+      factory().then((instance) => {
+        window.module = instance; // Assign to global variable
+        window.module.canvas = document.getElementById('imagePortCanvas');
+      });
+    });
+  } catch (error) {
+    console.error('Error initializing Emscripten module:', error);
+  }
+})();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
