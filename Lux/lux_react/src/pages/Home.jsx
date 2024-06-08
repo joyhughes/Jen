a5248d6df1083301b438316428a5ed2f19a27164
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-06-08 10:34:46 +0530

    Setting button added

--- Lux/lux_react/src/pages/Home.jsx
+++ Lux/lux_react/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import InterfaceContainer from "../components/InterfaceContainer";
-
+import Setting from "../components/SettingBtn/Setting";
 function Home() {
   const imgWidth = 512; // Replace with your image width
   const imgHeight = 512; // Replace with your image height
@@ -9,9 +9,9 @@ function Home() {
 
   return (
     <div
-      className="App"
-      style={{ height: "100vh", width: "100vw", overflow: "hidden" }}
+      style={{ height: "100vh", width: "100%",overflow: "hidden" }}
     >
+      <Setting panelSize={panelSize} />
       <InterfaceContainer ratio={ratio} panelSize={panelSize} />
     </div>
   );
